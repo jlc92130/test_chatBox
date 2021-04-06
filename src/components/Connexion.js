bc9931cd89300ecb1927da9f71e2076d036c8eb6
unknown <jl.crobeddu@codeur.online>
2021-04-06 19:59:26 +0200

    url de redirection

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Connexion extends Component {
     state = {
-        pseudo: ''
+        pseudo: '',
+        goToChat: false
     }
 
     handleChange = (e) => {
         const pseudo = e.target.value;
+        this.setState({pseudo})
     };
 
+    handleSubmit = (e) => {
+        e.preventDefault(),
+        this.setState({goToChat: true})
+    }
 
     render () {
+
+        if (this.state.goToChat) {
+            return <Redirect to={`/pseudo/${this.state.pseudo}`}></Redirect>
+        }
         return (
             <div className="connexionBox">
-                <form className="connexion">
+                <form className="connexion" onSubmit={this.handleSubmit}>
                     <input 
                     value = {this.state.pseudo}
                     onChange = {this.handleChange}
